1a4995c7cf097427e26690d87c80f423df851c41
Dennis Luxen <dluxen@bloomberg.net>
2021-08-03 17:50:05 +0200

    Remove member n from binary heap

--- src/binary_heap.rs
+++ src/binary_heap.rs
@@ -1,17 +1,16 @@
+// non-addressable priority queue
 pub struct BinaryHeap<T: Ord + Copy + Default> {
     h: Vec<T>,
-    n: usize,
 }
 
 impl<T: Ord + Copy + Default> BinaryHeap<T> {
     pub fn build(vec: &[T]) -> BinaryHeap<T> {
         let mut heap = BinaryHeap {
             h: vec.to_vec(),
-            n: vec.len(),
         };
 
-        // fix heap property
-        for i in 0..heap.n / 2 {
+        // set heap property
+        for i in 0..heap.len() / 2 {
             heap.sift_down(i);
         }
         heap
@@ -20,37 +19,38 @@ impl<T: Ord + Copy + Default> BinaryHeap<T> {
     pub fn new() -> BinaryHeap<T> {
         BinaryHeap {
             h: [T::default()].to_vec(),
-            n: 0,
         }
     }
 
     pub fn len(&self) -> usize {
-        self.n
+        self.h.len() - 1
     }
 
     pub fn insert(&mut self, value: T) {
-        self.n += 1;
         self.h.push(value);
-        self.sift_up(self.n);
+        self.sift_up(self.len());
     }
+
     pub fn min(&self) -> &T {
         &self.h[1]
     }
+
     pub fn delete_min(&mut self) -> T {
         let result = self.h[1];
-        self.h.swap(1, self.n);
-        self.n -= 1;
+        let last_index = self.h.len() -1;
+        self.h.swap(1, last_index);
+        self.h.pop();
         self.sift_down(1);
         result
     }
+
     pub fn is_empty(&self) -> bool {
-        self.n == 0
+        self.len() == 0
     }
+
     pub fn clear(&mut self) {
-        self.n = 0;
         self.h = vec![T::default()];
     }
-    // pub fn decrease_key() {}
 
     fn sift_up(&mut self, i: usize) {
         if i == 1 || self.h[i / 2] <= self.h[i] {
@@ -61,12 +61,12 @@ impl<T: Ord + Copy + Default> BinaryHeap<T> {
     }
 
     fn sift_down(&mut self, i: usize) {
-        if 2 * i > self.n {
+        if 2 * i > self.len() {
             return;
         }
 
         let mut m = 2 * i;
-        if 2 * i < self.n && self.h[2 * i] > self.h[2 * i + 1] {
+        if 2 * i < self.len() && self.h[2 * i] > self.h[2 * i + 1] {
             m += 1;
         }
 
