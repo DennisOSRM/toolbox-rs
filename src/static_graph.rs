00a985356a30784ddca3403685c929e4973fa6bf
Dennis Luxen <dluxen@bloomberg.net>
2021-08-04 12:24:11 +0200

    Move static graph tests to its implementation

--- src/static_graph.rs
+++ src/static_graph.rs
@@ -127,7 +127,7 @@ impl<T: Ord + Copy> StaticGraph<T> {
     pub fn get_out_degree(&self, n: NodeID) -> usize {
         let up = self.end_edges(n);
         let down = self.begin_edges(n);
-        (up - down ) as usize
+        (up - down) as usize
     }
 
     pub fn target(&self, e: EdgeID) -> NodeID {
@@ -182,5 +182,82 @@ impl<T: Ord + Copy> StaticGraph<T> {
             }
         }
         false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::static_graph::{InputEdge, StaticGraph};
+
+    #[test]
+    fn size() {
+        type Graph = StaticGraph<i32>;
+        let edges = vec![
+            InputEdge::new(0, 1, 3),
+            InputEdge::new(1, 2, 3),
+            InputEdge::new(4, 2, 1),
+            InputEdge::new(2, 3, 6),
+            InputEdge::new(0, 4, 2),
+            InputEdge::new(4, 5, 2),
+            InputEdge::new(5, 3, 7),
+            InputEdge::new(1, 5, 2),
+        ];
+        let graph = Graph::new(edges);
+
+        assert_eq!(6, graph.number_of_nodes());
+        assert_eq!(8, graph.number_of_edges());
+    }
+
+    #[test]
+    fn degree() {
+        type Graph = StaticGraph<i32>;
+        let edges = vec![
+            InputEdge::new(0, 1, 3),
+            InputEdge::new(1, 2, 3),
+            InputEdge::new(4, 2, 1),
+            InputEdge::new(2, 3, 6),
+            InputEdge::new(0, 4, 2),
+            InputEdge::new(4, 5, 2),
+            InputEdge::new(5, 3, 7),
+            InputEdge::new(1, 5, 2),
+        ];
+
+        let graph = Graph::new(edges);
+        let mut sum = 0;
+        for i in graph.node_range() {
+            sum += graph.get_out_degree(i);
+        }
+        assert_eq!(sum, graph.number_of_edges());
+    }
+
+    #[test]
+    fn no_cycle() {
+        type Graph = StaticGraph<i32>;
+        let edges = vec![
+            InputEdge::new(0, 1, 3),
+            InputEdge::new(1, 2, 3),
+            InputEdge::new(4, 2, 1),
+            InputEdge::new(2, 3, 6),
+            InputEdge::new(0, 4, 2),
+            InputEdge::new(4, 5, 2),
+            InputEdge::new(5, 3, 7),
+            InputEdge::new(1, 5, 2),
+        ];
+        let graph = Graph::new(edges);
+        assert_eq!(false, graph.cycle_check());
+    }
+
+    #[test]
+    fn cycle() {
+        type Graph = StaticGraph<i32>;
+        let edges = vec![
+            InputEdge::new(0, 1, 3),
+            InputEdge::new(2, 3, 3),
+            InputEdge::new(3, 4, 1),
+            InputEdge::new(4, 5, 6),
+            InputEdge::new(5, 2, 2),
+        ];
+        let graph = Graph::new(edges);
+        assert_eq!(true, graph.cycle_check());
     }
 }
