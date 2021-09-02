92c2d2fa39943f0a35ad42e3fc536976526d980c
Dennis Luxen <dluxen@bloomberg.net>
2021-09-02 15:01:29 +0200

    Implement Augmenting Path method

--- examples/ford_fulkerson.rs
+++ examples/ford_fulkerson.rs
@@ -38,9 +38,8 @@ fn main() {
     ];
     let number_of_edges = edges.len();
 
-    // duplicate edge set
+    // blindly generate reverse edges for all edges
     edges.extend_from_within(..);
-    // flag new edges as residual graphs edges
     edges.iter_mut().skip(number_of_edges).for_each(|edge| {
         edge.reverse();
         edge.data.capacity = 0;
@@ -48,48 +47,60 @@ fn main() {
     // dedup-merge edge set
     edges.sort();
     edges.dedup_by(|a, mut b| {
+        // merge duplicate edges by accumulating edge capacities
         let result = a.source == b.source && a.target == b.target;
         if result {
             b.data.capacity += a.data.capacity;
         }
         result
     });
+    // at this point the edge set doesn't have any duplicates anymore.
+    // note that this is fine, as we are looking to compute a node partition
 
-    println!("{:#?}", edges);
-    println!("len: {}, capacity: {}", edges.len(), edges.capacity());
+    // println!("{:#?}", edges);
+    // println!(
+    //     "edge list len: {}, capacity: {}",
+    //     edges.len(),
+    //     edges.capacity()
+    // );
 
     let mut graph = Graph::new(edges);
     let mut bfs = BFS::new();
     let filter = |graph: &Graph, edge| graph.data(edge).capacity <= 0;
     while bfs.run_with_filter(&graph, vec![0], vec![5], filter) {
-        // retrieve path
+        // retrieve node path. This is sufficient, as we removed all duplicate edges
         let path = bfs.fetch_node_path();
-        assert_eq!(path, vec![0, 1, 3, 5]);
         println!("found node path {:#?}", path);
-        let path = bfs.fetch_edge_path(&graph);
-        println!("found edge path {:#?}", path);
 
         // find min capacity on edges of the path
-        let least_capacity_edge = *path
-            .iter()
-            .min_by_key(|x| graph.data(**x).capacity)
+        let st_tuple = path
+            .windows(2)
+            .min_by_key(|window| {
+                let edge = graph.find_edge(window[0], window[1]).unwrap();
+                graph.data(edge).capacity
+            })
             .unwrap();
-        let current_flow = graph.data(least_capacity_edge).capacity;
-        assert!(current_flow > 0);
+
+        let fwd_least_capacity_edge = graph.find_edge(st_tuple[0], st_tuple[1]).unwrap();
+        let path_flow = graph.data(fwd_least_capacity_edge).capacity;
+        assert!(path_flow > 0);
         println!(
             "min edge: {}, capacity: {}",
-            least_capacity_edge, current_flow
+            fwd_least_capacity_edge, path_flow
         );
 
-        // todo(dluxen): assign flow to Graph
-        path.iter().for_each(|e| {
-            graph.data_mut(*e).capacity -= current_flow;
-            // let reverse_edge = graph .find_edge(s, t)
+        // assign flow to residual graph
+        path.windows(2).for_each(|pair| {
+            let fwd_edge = graph.find_edge(pair[0], pair[1]).unwrap();
+            let rev_edge = graph.find_edge(pair[1], pair[0]).unwrap();
+
+            graph.data_mut(fwd_edge).capacity -= path_flow;
+            graph.data_mut(rev_edge).capacity += path_flow;
         });
-        // break;
     }
 
-    // todo(dluxen): retrieve cut
+    // todo(dluxen): retrieve max-flow
+    // todo(dluxen): retrieve min-cut
 
     println!("done.")
 }
