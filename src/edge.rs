bd559deb1c247eae1d0b99abf87b57c8ab92a105
Dennis Luxen <info@project-osrm.org>
2021-12-19 13:34:12 +0100

    Implement Edge trait

--- src/edge.rs
+++ src/edge.rs
@@ -1,11 +1,33 @@
 use crate::graph::NodeID;
 use std::mem::swap;
 
+pub trait Edge {
+    type ID;
+    type DATA;
+    fn source(&self) -> Self::ID;
+    fn target(&self) -> Self::ID;
+    fn data(&self) -> &Self::DATA;
+}
+
 #[derive(Clone, Copy, Debug, Default, Eq, PartialOrd, Ord, PartialEq)]
 pub struct InputEdge<EdgeDataT: Eq> {
     pub source: NodeID,
     pub target: NodeID,
     pub data: EdgeDataT,
+}
+
+impl<EdgeDataT: std::cmp::Eq> Edge for InputEdge<EdgeDataT> {
+    type ID = NodeID;
+    type DATA = EdgeDataT;
+    fn source(&self) -> Self::ID {
+        self.source
+    }
+    fn target(&self) -> Self::ID {
+        self.target
+    }
+    fn data(&self) -> &Self::DATA {
+        &self.data
+    }
 }
 
 impl<EdgeDataT: Eq> InputEdge<EdgeDataT> {
