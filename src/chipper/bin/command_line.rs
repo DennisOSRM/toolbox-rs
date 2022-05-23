c7bbb095695af1fcf13680b88bb1ce6df08581ed
Dennis Luxen <info@project-osrm.org>
2022-05-23 15:47:07 +0200

    Implement recursive partitioning (#67)

--- src/chipper/bin/command_line.rs
+++ src/chipper/bin/command_line.rs
@@ -1,10 +1,10 @@
 use std::ops::RangeInclusive;
 
-const RECURSION_RANGE: RangeInclusive<usize> = 1..=15;
+const RECURSION_RANGE: RangeInclusive<u8> = 1..=31;
 
-/// Checks whether the target level is within the expected range of (1, 15].
-pub fn recursion_in_range(s: &str) -> Result<usize, String> {
-    let recursion: usize = s.parse().map_err(|_| format!("`{}` isn't a number", s))?;
+/// Checks whether the target level is within the expected range of (1, 31].
+pub fn recursion_in_range(s: &str) -> Result<u8, String> {
+    let recursion: u8 = s.parse().map_err(|_| format!("`{}` isn't a number", s))?;
     if RECURSION_RANGE.contains(&recursion) {
         Ok(recursion)
     } else {
