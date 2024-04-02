1fa40e58417e675109d3c265652fcb6c1a91caba
Dennis Luxen <info@project-osrm.org>
2024-04-02 20:31:46 +0200

    Add test coverage: space_filling_curve.rs

--- src/space_filling_curve.rs
+++ src/space_filling_curve.rs
@@ -13,3 +13,30 @@ pub fn zorder_cmp(lhs: FPCoordinate, rhs: FPCoordinate) -> std::cmp::Ordering {
 fn less_msb(x: i32, y: i32) -> bool {
     x < y && x < (x ^ y)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{geometry::primitives::FPCoordinate, space_filling_curve::zorder_cmp};
+
+    #[test]
+    fn compare_greater() {
+        let ny = FPCoordinate::new_from_lat_lon(40.730610, -73.935242);
+        let sf = FPCoordinate::new_from_lat_lon(37.773972, -122.431297);
+
+        assert_eq!(std::cmp::Ordering::Greater, zorder_cmp(ny, sf));
+    }
+
+    #[test]
+    fn compare_less() {
+        let ny = FPCoordinate::new_from_lat_lon(40.730610, -73.935242);
+        let sf = FPCoordinate::new_from_lat_lon(37.773972, -122.431297);
+
+        assert_eq!(std::cmp::Ordering::Less, zorder_cmp(sf, ny));
+    }
+
+    #[test]
+    fn compare_equal() {
+        let ny = FPCoordinate::new_from_lat_lon(40.730610, -73.935242);
+        assert_eq!(std::cmp::Ordering::Equal, zorder_cmp(ny, ny));
+    }
+}
