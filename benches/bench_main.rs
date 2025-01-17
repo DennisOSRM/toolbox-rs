e3ef93bec9ccd0a70169d244cd13d66076291853
Dennis Luxen <info@project-osrm.org>
2025-01-17 19:26:14 +0100

    Implement range query for Fenwick tree (#440)
    * Implement range query for Fenwick tree
    
    * Add forgotten benchmark
    
    * Fix formatting

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -3,6 +3,7 @@ use criterion::criterion_main;
 mod benchmarks;
 
 criterion_main!(
+    benchmarks::fenwick::all_fenwick,
     benchmarks::great_circle::distances,
     benchmarks::radix_sort::all_sorts
 );
