c056f89017cd2857ac1597775fa1b077c4ca9e7d
Dennis Luxen <ich@dennisluxen.de>
2025-02-01 14:09:06 +0100

    Fix DDSG parsing, add more unit tests to cover data parsing`

--- src/ddsg.rs
+++ src/ddsg.rs
@@ -8,6 +8,7 @@ pub enum WeightType {
     Original,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Direction {
     Both = 0,
     Forward = 1,
@@ -29,7 +30,7 @@ impl TryFrom<i32> for Direction {
     }
 }
 
-pub fn read_graph<T: std::cmp::Eq + From<usize>>(
+pub fn read_graph<T: std::fmt::Debug + std::cmp::Eq + From<usize>>(
     filename: &str,
     weight_type: WeightType,
 ) -> Vec<InputEdge<T>> {
@@ -60,11 +61,11 @@ pub fn read_graph<T: std::cmp::Eq + From<usize>>(
         if tokens.len() != 4 {
             continue;
         }
-        let source = tokens[0].parse::<NodeID>().unwrap();
-        let target = tokens[1].parse::<NodeID>().unwrap();
+        let u = tokens[0].parse::<NodeID>().unwrap();
+        let v = tokens[1].parse::<NodeID>().unwrap();
 
         // avoid eigenloops
-        if source == target {
+        if u == v {
             continue;
         }
 
@@ -75,16 +76,16 @@ pub fn read_graph<T: std::cmp::Eq + From<usize>>(
         match direction {
             Direction::Both => {
                 edges.push(InputEdge::<T> {
-                    source,
-                    target,
+                    source: u,
+                    target: v,
                     data: match &weight_type {
                         WeightType::Unit => T::from(1),
                         WeightType::Original => T::from(data),
                     },
                 });
                 edges.push(InputEdge::<T> {
-                    target,
-                    source,
+                    source: v,
+                    target: u,
                     data: match &weight_type {
                         WeightType::Unit => T::from(1),
                         WeightType::Original => T::from(data),
@@ -93,8 +94,8 @@ pub fn read_graph<T: std::cmp::Eq + From<usize>>(
             }
             Direction::Forward => {
                 edges.push(InputEdge::<T> {
-                    source,
-                    target,
+                    source: u,
+                    target: v,
                     data: match &weight_type {
                         WeightType::Unit => T::from(1),
                         WeightType::Original => T::from(data),
@@ -103,8 +104,8 @@ pub fn read_graph<T: std::cmp::Eq + From<usize>>(
             }
             Direction::Reverse => {
                 edges.push(InputEdge::<T> {
-                    target,
-                    source,
+                    source: v,
+                    target: u,
                     data: match &weight_type {
                         WeightType::Unit => T::from(1),
                         WeightType::Original => T::from(data),
@@ -143,4 +144,87 @@ pub fn read_coordinates(filename: &str) -> Vec<FPCoordinate> {
     info!("loaded {coordinate_count} coordinates");
 
     coordinates
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    use crate::ddsg::{read_coordinates, Direction, WeightType};
+
+    #[test]
+    fn direction_try_from() {
+        assert_eq!(Direction::try_from(0), Ok(Direction::Both));
+        assert_eq!(Direction::try_from(1), Ok(Direction::Forward));
+        assert_eq!(Direction::try_from(2), Ok(Direction::Reverse));
+        assert_eq!(Direction::try_from(3), Ok(Direction::Closed));
+        assert_eq!(Direction::try_from(4), Err(()));
+    }
+
+    #[test]
+    fn read_graph_undirected() {
+        let filename = create_temp_file_with_content("d\n2 1\n0 1 10 0\n");
+        let edges = crate::ddsg::read_graph::<usize>(
+            filename.path().to_str().unwrap(),
+            WeightType::Original,
+        );
+        assert_eq!(edges.len(), 2);
+        assert_eq!(edges[0].source, 0);
+        assert_eq!(edges[0].target, 1);
+        assert_eq!(edges[0].data, 10);
+        assert_eq!(edges[1].source, 1);
+        assert_eq!(edges[1].target, 0);
+        assert_eq!(edges[1].data, 10);
+    }
+
+    #[test]
+    fn read_graph_with_invalid_data() {
+        let filename = create_temp_file_with_content("t\n2 1\n0 1 10 0\n");
+        let edges = crate::ddsg::read_graph::<usize>(
+            filename.path().to_str().unwrap(),
+            WeightType::Original,
+        );
+        assert_eq!(edges.len(), 0);
+    }
+
+    #[test]
+    fn read_graph_with_different_directions() {
+        let filename = create_temp_file_with_content("d\n3 3\n0 1 10 1\n1 2 20 2\n2 0 30 3\n");
+        let edges = crate::ddsg::read_graph::<usize>(
+            filename.path().to_str().unwrap(),
+            WeightType::Original,
+        );
+        assert_eq!(edges.len(), 2);
+        assert_eq!(edges[0].source, 0);
+        assert_eq!(edges[0].target, 1);
+        assert_eq!(edges[0].data, 10);
+        assert_eq!(edges[1].source, 2);
+        assert_eq!(edges[1].target, 1);
+        assert_eq!(edges[1].data, 20);
+    }
+
+    #[test]
+    fn read_ddsg_coordinates() {
+        let filename = create_temp_file_with_content("2\n0 1000000 2000000\n1 3000000 4000000\n");
+        let coordinates = crate::ddsg::read_coordinates(filename.path().to_str().unwrap());
+        assert_eq!(coordinates.len(), 2);
+        assert_eq!(coordinates[0].to_lon_lat_pair(), (10., 20.));
+        assert_eq!(coordinates[1].to_lon_lat_pair(), (30., 40.));
+    }
+
+    #[test]
+    fn read_coordinates_with_invalid_data() {
+        let filename =
+            create_temp_file_with_content("2\n0 1000000 2000000\n1 invalid_data 4000000\n");
+        let result =
+            std::panic::catch_unwind(|| read_coordinates(filename.path().to_str().unwrap()));
+        assert!(result.is_err());
+    }
+
+    fn create_temp_file_with_content(content: &str) -> NamedTempFile {
+        let mut file = NamedTempFile::new().unwrap();
+        write!(file, "{}", content).unwrap();
+        file
+    }
 }
