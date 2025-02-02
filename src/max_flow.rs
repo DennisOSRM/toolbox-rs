30f149a7b5bdc7ed44dbd4ca69a5631e457d0793
Dennis Luxen <info@project-osrm.org>
2025-02-02 14:55:11 +0100

    Move common functionality into MaxFlow trait implementation (#456)

--- src/max_flow.rs
+++ src/max_flow.rs
@@ -1,7 +1,11 @@
 use std::sync::{atomic::AtomicI32, Arc};
 
-use crate::graph::NodeID;
+use crate::{
+    edge::{EdgeWithData, InputEdge},
+    graph::NodeID,
+};
 use bitvec::vec::BitVec;
+use log::debug;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ResidualEdgeData {
@@ -23,4 +27,32 @@ pub trait MaxFlow {
     fn run_with_upper_bound(&mut self, bound: Arc<AtomicI32>);
     fn max_flow(&self) -> Result<i32, String>;
     fn assignment(&self, source: NodeID) -> Result<BitVec, String>;
+    fn from_edge_list(
+        edges: Vec<InputEdge<ResidualEdgeData>>,
+        source: NodeID,
+        sink: NodeID,
+    ) -> Self;
+    fn from_generic_edge_list<E: EdgeWithData>(
+        input_edges: &[E],
+        source: NodeID,
+        target: NodeID,
+        function: impl Fn(&E) -> ResidualEdgeData,
+    ) -> Self
+    where
+        Self: Sized,
+    {
+        debug_assert!(!input_edges.is_empty());
+        debug!("instantiating max-flow solver");
+        let edge_list: Vec<InputEdge<ResidualEdgeData>> = input_edges
+            .iter()
+            .map(move |edge| InputEdge {
+                source: edge.source(),
+                target: edge.target(),
+                data: function(edge),
+            })
+            .collect();
+
+        debug!("created {} ff edges", edge_list.len());
+        Self::from_edge_list(edge_list, source, target)
+    }
 }
