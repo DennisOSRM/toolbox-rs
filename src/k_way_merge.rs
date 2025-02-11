f961e9fe1fb4623cccb6b769c7d30d2bc14d15c1
Dennis Luxen <info@project-osrm.org>
2025-02-11 19:42:06 +0100

    Factor out contained item into generic parameter

--- src/k_way_merge.rs
+++ src/k_way_merge.rs
@@ -1,34 +1,51 @@
 use std::iter::Iterator;
 use std::{cmp::Reverse, collections::BinaryHeap};
 
-pub struct KWayMergeIterator<'a, T: Iterator<Item = i32>> {
-    heap: BinaryHeap<Reverse<(i32, usize)>>,
-    list: &'a mut [T],
+// TODO: add trait to represent merge tree
+// trait MergeTree<T> {
+//     fn push(&mut self, t: T);
+//     fn pop(&mut self) -> Option<T>;
+// }
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Entry<T> {
+    item: T,
+    index: usize,
 }
 
-impl<'a, T: Iterator<Item = i32>> KWayMergeIterator<'a, T> {
-    pub fn new(list: &'a mut [T]) -> Self {
+pub struct KWayMergeIterator<'a, T, I: Iterator<Item = T>> {
+    heap: BinaryHeap<Reverse<Entry<T>>>,
+    list: &'a mut [I],
+}
+
+impl<'a, T: std::cmp::Ord, I: Iterator<Item = T>> KWayMergeIterator<'a, T, I> {
+    pub fn new(list: &'a mut [I]) -> Self {
         let mut heap = BinaryHeap::new();
         for (i, iterator) in list.iter_mut().enumerate() {
             if let Some(first) = iterator.next() {
-                heap.push(Reverse((first, i)));
+                heap.push(Reverse(Entry {
+                    item: first,
+                    index: i,
+                }));
             }
         }
         Self { heap, list }
     }
-
-    pub fn has_next(&self) -> bool {
-        !self.heap.is_empty()
-    }
 }
 
-impl<T: Iterator<Item = i32>> Iterator for KWayMergeIterator<'_, T> {
-    type Item = i32;
+impl<T: std::cmp::Ord, I: Iterator<Item = T>> Iterator for KWayMergeIterator<'_, T, I> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let Reverse((value, list)) = self.heap.pop()?;
+        let Reverse(Entry {
+            item: value,
+            index: list,
+        }) = self.heap.pop()?;
         if let Some(next) = self.list[list].next() {
-            self.heap.push(Reverse((next, list)));
+            self.heap.push(Reverse(Entry {
+                item: next,
+                index: list,
+            }));
         }
         Some(value)
     }
@@ -50,5 +67,25 @@ mod test {
             result,
             vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         );
+    }
+
+    #[test]
+    fn three_way_merge_of_differently_sized_sequences() {
+        let mut list = vec![
+            vec![1, 3, 5, 6, 7, 12].into_iter(),
+            vec![2, 4, 8, 11].into_iter(),
+            vec![9, 10].into_iter(),
+        ];
+        let k_way_merge = super::KWayMergeIterator::new(&mut list);
+        let result: Vec<_> = k_way_merge.collect();
+        assert_eq!(result, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    }
+
+    #[test]
+    fn merge_of_empty_sequences() {
+        let mut list: Vec<std::vec::IntoIter<u64>> = vec![vec![].into_iter(), vec![].into_iter()];
+        let k_way_merge = super::KWayMergeIterator::new(&mut list);
+        let result: Vec<_> = k_way_merge.collect();
+        assert_eq!(result, Vec::<u64>::new());
     }
 }
