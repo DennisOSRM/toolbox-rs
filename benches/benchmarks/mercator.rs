95c01cdd90b2b5de78467b7d03f48e5d631993d8
Dennis Luxen <info@project-osrm.org>
2025-03-15 19:17:49 +0100

    Add benchmarks

---
+++ benches/benchmarks/mercator.rs
@@ -0,0 +1,57 @@
+use criterion::{BenchmarkId, Criterion, black_box, criterion_group};
+use rand::Rng;
+use toolbox_rs::{
+    mercator::{lat_to_y, lat_to_y_approx, lon_to_x},
+    wgs84::{FloatLatitude, FloatLongitude},
+};
+
+pub fn lat_to_y_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lat_to_y_sizes");
+    let sizes = [10, 100, 1000, 10000];
+    let mut rng = rand::rng();
+
+    for size in sizes {
+        let latitudes: Vec<FloatLatitude> = (0..size)
+            .map(|_| FloatLatitude(rng.random_range(-70.0..70.0)))
+            .collect();
+
+        group.bench_with_input(BenchmarkId::new("exact", size), &latitudes, |b, lats| {
+            b.iter(|| {
+                for &lat in lats.iter() {
+                    black_box(lat_to_y(lat));
+                }
+            })
+        });
+
+        group.bench_with_input(BenchmarkId::new("approx", size), &latitudes, |b, lats| {
+            b.iter(|| {
+                for &lat in lats.iter() {
+                    black_box(lat_to_y_approx(lat));
+                }
+            })
+        });
+    }
+    group.finish();
+}
+
+pub fn lon_to_x_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lon_to_x_sizes");
+    let sizes = [10, 100, 1000, 10000];
+    let mut rng = rand::rng();
+
+    for size in sizes {
+        let longitudes: Vec<FloatLongitude> = (0..size)
+            .map(|_| FloatLongitude(rng.random_range(-70.0..70.0)))
+            .collect();
+
+        group.bench_with_input(BenchmarkId::new("exact", size), &longitudes, |b, lons| {
+            b.iter(|| {
+                for &lon in lons.iter() {
+                    black_box(lon_to_x(lon));
+                }
+            })
+        });
+    }
+    group.finish();
+}
+criterion_group!(mercator_benches, lat_to_y_benchmark, lon_to_x_benchmark);
